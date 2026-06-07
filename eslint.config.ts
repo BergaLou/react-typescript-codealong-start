687574ce74cd926fa3df21a72032461fb9ef83a1
BergaLou <157978333+BergaLou@users.noreply.github.com>
2026-06-07 18:23:38 +0200

    struggle

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,8 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import typescript from '@typescript-eslint/eslint/plugin';
+import typescriptParser from '@typescript-eslint/parser';
 
 export default [
   { ignores: ['dist'] },
